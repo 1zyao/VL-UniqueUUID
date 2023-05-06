2958019888b1c42ef3f9902529198b7edcb10f7b
1zyao <liuziyao20191212@163.com>
2023-05-07 07:38:48 +0800

    Update UniqueUUID.java

--- src/main/java/site/vstl/UniqueUUID/UniqueUUID.java
+++ src/main/java/site/vstl/UniqueUUID/UniqueUUID.java
@@ -4,43 +4,65 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.io.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Level;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
+
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.OutputKeys;
+
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class UniqueUUID extends JavaPlugin {
     private static Map<String, UUID> name2uuid;
     private static Map<UUID, String> uuid2name;
+    private File data;
 
     @Override
     public void onEnable() {
-        File data = new File(getDataFolder(), "storage.bin");
+        data = new File(getDataFolder(), "storage.xml");
         if (data.isFile()) {
-            try (FileInputStream fileInputStream = new FileInputStream(data);
-                 BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
-                 GZIPInputStream gzipInputStream = new GZIPInputStream(bufferedInputStream);
-                 DataInputStream bin = new DataInputStream(gzipInputStream)
-            ) {
-                int size = bin.readInt();
-                name2uuid = new HashMap<>(size);
-                uuid2name = new HashMap<>(size);
-                while (size-- > 0) {
-                    UUID uid = new UUID(bin.readLong(), bin.readLong());
-                    String usr = bin.readUTF();
+            try {
+                name2uuid = new HashMap<>(255);
+                uuid2name = new HashMap<>(255);
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document doc = db.parse(data);
+                doc.getDocumentElement().normalize();
+                NodeList nodeList = doc.getElementsByTagName("player");
+                for (int i = 0; i < nodeList.getLength(); i++) {
+                    Element element = (Element) nodeList.item(i);
+                    UUID uid = UUID.fromString(element.getAttribute("uuid"));
+                    String usr = element.getAttribute("name");
                     name2uuid.put(usr, uid);
                     uuid2name.put(uid, usr);
                 }
-            } catch (Throwable any) {
-                getLogger().log(Level.SEVERE, "Exception in reading bin database");
+            } catch (ParserConfigurationException | SAXException e) {
+                getLogger().log(Level.SEVERE, "Exception in parsing xml database");
+            } catch (Exception e) {
+                getLogger().log(Level.SEVERE, "Exception in reading xml database");
             }
         } else {
             name2uuid = new HashMap<>(255);
             uuid2name = new HashMap<>(255);
         }
+
         getServer().getPluginManager().registerEvents(new LoginListener(), this);
         getServer().getScheduler().scheduleSyncRepeatingTask(this, this::save, 0, 10 * 20 * 60);
         getLogger().log(Level.INFO, "Enabled");
@@ -84,32 +106,44 @@ public class UniqueUUID extends JavaPlugin {
     }
 
     @Override
-    public void onDisable() {
+    public void onDisable() { 
         save();
     }
 
     private void save() {
-        Map<UUID, String> copied;
-        copied = new HashMap<>(uuid2name);
         try {
-            File data = new File(getDataFolder(), "storage.bin");
-            getDataFolder().mkdirs();
-            try (FileOutputStream fileOutputStream = new FileOutputStream(data);
-                 BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
-                 GZIPOutputStream gzipOutputStream = new GZIPOutputStream(bufferedOutputStream);
-                 DataOutputStream bin = new DataOutputStream(gzipOutputStream)
-            ) {
-                bin.writeInt(copied.size());
-                for (Map.Entry<UUID, String> entry : copied.entrySet()) {
-                    UUID uid = entry.getKey();
-                    bin.writeLong(uid.getMostSignificantBits());
-                    bin.writeLong(uid.getLeastSignificantBits());
-                    bin.writeUTF(entry.getValue());
-                }
+            data.getParentFile().mkdirs();
+            data.createNewFile();
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.newDocument();
+            Element root = doc.createElement("players");
+            doc.appendChild(root);
+            for (Map.Entry<String, UUID> entry : name2uuid.entrySet()) {
+                Element player = doc.createElement("player");
+                player.setAttribute("name", entry.getKey());
+                player.setAttribute("uuid", entry.getValue().toString());
+                root.appendChild(player);
             }
+            FileOutputStream fos = new FileOutputStream(data);
+            fos.write(docToString(doc).getBytes());
+            fos.close();
             getLogger().log(Level.INFO, "Saved");
-        } catch (IOException ioException) {
-            getLogger().log(Level.SEVERE, "Exception in saving storage");
+        } catch (Exception e) {
+            getLogger().log(Level.SEVERE, "Exception in saving xml database");
+        }
+    }
+
+    private String docToString(Document document) {
+        try {
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            StringWriter writer = new StringWriter();
+            transformer.transform(new DOMSource(document), new StreamResult(writer));
+            return writer.getBuffer().toString();
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting Document to String", e);
         }
     }
 }
